5d0a21afd11f738ce5e1faa49e79a725c9d61116
bkedan <EThany49@nycstudents.net>
2025-03-27 10:14:09 -0400

    PLEASE HELP

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import json
 
-class student:
+class Student:
     def __init__(self, name, classes, homework):
         self.name = name
         self.classes = classes
         self.homework = homework
     def display_info(self):
+        return f"{self.name} {self.classes} {self.homework}"
+def to_dict(self):
+    return {"name": self.name, "classes": self.classes, "homework": self.homework}
+student = Student("Ethan", "Geometry", "Trig test")
 
+my_student = Student("Ethan", "SITHS", 2025,)
+print(my_student.display_info())
 
 
 
