42766db1f0e4b2e25957793d624bb67f6b3d23f9
bkedan <EThany49@nycstudents.net>
2025-04-02 10:14:38 -0400

    e

--- app.py
+++ app.py
@@ -7,15 +7,9 @@ class Student:
         self.homework = homework
     def display_info(self):
         return f"{self.name} {self.classes} {self.homework}"
-def to_dict(self):
-    return {"name": self.name, "classes": self.classes, "homework": self.homework}
-student = Student("Ethan", "Geometry", "Trig test")
 
-my_student = Student("Ethan", "SITHS", 2025,)
+    def to_dict(self):
+        return {"name": self.name, "classes": self.classes, "homework": self.homework}
+
+my_student = Student("Ethan", "SITHS", 2028)
 print(my_student.display_info())
-
-
-
-
-
-
