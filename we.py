1ba0e988893c18329a4aa5fc93c52f413a90efb6
bkedan <EThany49@nycstudents.net>
2025-04-08 10:14:55 -0400

    Update we.py

--- we.py
+++ we.py
@@ -1,10 +1,27 @@
+import json
+
 class teach:
     def __init__(self):
         self.aa = {}
+
     def makekey(self):
-        key = input("give key: ")
-        value = input("give value: ")
-        self.aa[key] = value 
+        key = input("Give key: ")
+        value = input("Give value: ")
+        self.aa[key] = value
         print(self.aa)
+
+    def save(self):
+        try:
+            with open("FlashCards.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = []  
+
+        data.append(self.aa)
+
+        with open("FlashCards.json", "w") as file:
+            json.dump(data, file, indent=4)
+
 w = teach()
 w.makekey()
+w.save()
